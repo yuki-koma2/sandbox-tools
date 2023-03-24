a9e1581fca4cc4a9156fefecf47ced82f4f9cd66
yukikoma <yuki.k.lion@gmail.com>
2023-03-24 17:57:52 +0900

    openSSL関連でエラーが発生。まだNuxt3のサポートがないので、一旦ここまで。対応されたら続きをやる。

--- nuxt-sand/.storybook/main.js
+++ nuxt-sand/.storybook/main.js
@@ -1,5 +1,7 @@
+// main.js
+
 module.exports = {
-    stories: ['../components/stories/**/*.stories.mdx', '../components/stories/**/*.stories.@(js|jsx|ts|tsx)'],
+    stories: ['../components/stories/**/*.stories.mdx', '../components/stories/**/*.stories.@(js|jsx|ts|tsx|vue)'],
     addons: ['@storybook/addon-essentials'],
     framework: '@storybook/vue3',
     core: {
