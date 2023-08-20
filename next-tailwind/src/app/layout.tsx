0a219e42fab467fbd025543095a5c268fa3f44bf
yuki-koma3 <129825800+yukikoma3@users.noreply.github.com>
2023-08-20 12:58:04 +0900

    導入はできた

--- next-tailwind/src/app/layout.tsx
+++ next-tailwind/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Providers } from "@/app/providers";
+import { Button } from "@nextui-org/button";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+      // <html lang="en" className='dark'>
+      <html lang="en">
+      <body className={inter.className}>
+      <Providers>
+          <Button>Click me</Button>
+          {children}
+      </Providers>
+      </body>
     </html>
   )
 }
